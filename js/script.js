eb714c6ba9453fda1bfc6eb2bea08ea0ba860bf3
Yosber Guerrero <yosbergc@gmail.com>
2023-11-15 16:21:37 -0400

    Mobile desktop version

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-// Add some interactivity to the website
-console.log("Hello, world!");
+const menuIcon = document.querySelector('.hamburgerMenuIcon');
+const mobileMenu = document.querySelector('.hamburgerMenu');
+const closeIcon = document.querySelector('.closeIcon')
+const darkModeIcon = document.querySelector('.dark-mode')
+const lightModeIcon = document.querySelector('.light-mode')
+menuIcon.addEventListener('click', toggleMenu);
+closeIcon.addEventListener('click', toggleMenu);
+window.addEventListener('resize', mobileClose);
+
+function mobileClose() {
+    if (window.innerWidth >= 1024) {
+        mobileMenu.classList.add('closed');
+    }
+}
+function toggleMenu() {
+    if (mobileMenu.classList.contains('closed')) {
+        mobileMenu.classList.remove('closed');
+    } else {
+        mobileMenu.classList.add('closed');
+    }
+}
